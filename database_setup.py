fe6a94f083db86878fe899c30865fdd426d100b6
Danny Peck <dannytpeck@gmail.com>
2017-10-02 13:04:59 -0700

    Add users to DB

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -5,11 +6,24 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    item = relationship('Item', cascade='all, delete-orphan')
 
     @property
     def serialize(self):
@@ -19,14 +33,17 @@ class Category(Base):
             'name': self.name
         }
 
+
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(500))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -36,6 +53,7 @@ class Item(Base):
             'name': self.name,
             'description': self.description
         }
+
 
 engine = create_engine('sqlite:///itemlist.db')
 
